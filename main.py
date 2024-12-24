ba7549d4c798fe3c04bc0d38687dd5acfcdee5f8
Kharisma Gilang Perdana <134329729+jonoo0@users.noreply.github.com>
2024-12-24 20:09:00 +0000

    change port to 80

--- main.py
+++ main.py
@@ -72,4 +72,4 @@ async def get_kode_wilayah(kode: str) -> str:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
