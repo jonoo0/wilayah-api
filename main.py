284b2083386496437e8b6a698711fd460d18f3ac
KharismaGP <134329729+jonoo0@users.noreply.github.com>
2024-12-25 14:27:25 +0000

    remove main func

--- main.py
+++ main.py
@@ -70,6 +70,6 @@ async def get_kode_wilayah(kode: str) -> str:
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=80)
